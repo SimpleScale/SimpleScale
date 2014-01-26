9cfc14d0e4a9144ad4a8c37af0012d767352385a
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-01-26 17:40:49 +0000

    Created simple queue manager and map job

--- HeadNode/Class1.cs
+++ HeadNode/Class1.cs
@@ -1,11 +1,72 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace SimpleScale.HeadNode
 {
-    public class Class1
+    public class Job<T>
     {
+        private int _Id;
+        private T _jobInfo;
+        public Job(int id, T jobInfo)
+        {
+            _Id = id;
+            _jobInfo = jobInfo;
+        }
+    }
+
+    public interface IQueueManager<T>
+    {
+        void Add(List<Job<T>> jobs);
+        Job<T> Read();
+    }
+
+    public class MemoryQueueManager<T> : IQueueManager<T>
+    {
+        private ConcurrentQueue<Job<T>> _jobs = new ConcurrentQueue<Job<T>>();
+        public MemoryQueueManager() {}
+
+        public void Add(List<Job<T>> jobs)
+        {
+            jobs.ForEach(j => _jobs.Enqueue(j));
+        }
+
+        public Job<T> Read()
+        {
+            Job<T> returnValue;
+            while (true)
+            {
+                if (_jobs.TryDequeue(out returnValue))
+                    return returnValue;
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    public interface IMapJob<T> {
+        void DoWork(Job<T> job);
+    }
+
+    public class MapService<T>
+    {
+        readonly IQueueManager<T> _queueManager;
+        readonly IMapJob<T> _mapJob;
+        public MapService(IQueueManager<T> queueManager, IMapJob<T> mapJob)
+        {
+            _queueManager = queueManager;
+            _mapJob = mapJob;
+        }
+
+        public void Start()
+        {
+            while (true){
+                var job = _queueManager.Read();
+                _mapJob.DoWork(job);
+            }
+        }
+
     }
 }
