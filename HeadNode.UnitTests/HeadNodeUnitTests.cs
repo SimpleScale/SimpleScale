5eb173c574c2a9e5d76c501f7c13d2c938161b2f
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-02-02 21:08:47 +0000

    Add constructor test to head node unit tests

---
+++ HeadNode.UnitTests/HeadNodeUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace SimpleScale.HeadNode.UnitTests
+{
+    [TestFixture]
+    public class HeadNodeUnitTests
+    {
+        [Test]
+        public void TestCTor()
+        {
+            var queueManager = new Common.MemoryQueueManager<int>();
+            var headNode = new HeadNode.HeadNode<int>(queueManager);
+        }
+    }
+}
