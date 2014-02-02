0d71f3feb9c27ae46db92c63b28039d79b12295c
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-02-02 18:28:39 +0000

    Add pack nuget batch file to solution

---
+++ Pack Nuget.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe HeadNode.csproj /p:Configuration=Release
+nuget pack HeadNode\HeadNode.csproj -IncludeReferencedProjects -Prop Configuration=Release
