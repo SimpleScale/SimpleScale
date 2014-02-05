bce567b54879b8014bf92b57def6c2b17d5ebb72
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-02-05 22:00:33 +0000

    Add BatchCompleteEventArgs

---
+++ HeadNode/BatchCompleteEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleScale.HeadNode
+{
+    public class BatchCompleteEventArgs : EventArgs
+    {
+        public readonly Guid BatchID;
+        public BatchCompleteEventArgs(Guid batchId)
+        {
+            BatchID = batchId;
+        }
+    }
+}
