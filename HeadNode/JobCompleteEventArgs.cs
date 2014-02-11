f01042b4135eb4bc2a2eb6ac81772cecfa72903c
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-02-11 20:40:18 +0000

    Added job complete event args

---
+++ HeadNode/JobCompleteEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SimpleScale.Common;
+
+namespace SimpleScale.HeadNode
+{
+    public class JobCompleteEventArgs<U> : EventArgs
+    {
+        public readonly Result<U> Result;
+
+        public JobCompleteEventArgs(Result<U> result)
+        {
+            Result = result;
+        }
+    }
+}
