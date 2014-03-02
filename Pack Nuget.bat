2e3f9d3fdce03722c06dd04a94f2029092c7a5da
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2014-03-02 12:25:43 +0000

    Clean before build

--- Pack Nuget.bat
+++ Pack Nuget.bat
@@ -1,3 +1,3 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe HeadNode/HeadNode.csproj /p:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /t:Clean,Build HeadNode/HeadNode.csproj /p:Configuration=Release
 nuget pack HeadNode\HeadNode.csproj -IncludeReferencedProjects -Prop Configuration=Release
 pause
